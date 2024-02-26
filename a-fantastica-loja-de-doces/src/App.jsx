372b8ef9df9795b01c4250de649c3f05f017581b
Lorrayne <lorraynelimamota@gmail.com>
2024-02-26 05:39:19 -0300

    funcionalidade de routes ajustada e funcionando

--- a-fantastica-loja-de-doces/src/App.jsx
+++ a-fantastica-loja-de-doces/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import AppRoutes from './Components/Router/Routes.jsx'
 import Header from './Components/Header/Header.jsx'
+import Home from './Components/Main/Home/Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <Home>
     <h6>Pagina Inicial?</h6>
     <Header/>
     <AppRoutes/>
-    </div>
+    </Home>
   )
 }
 
